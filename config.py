2625ab2d1f12bb39de1ba6574d22850c67d9b817
Takeo Shibata <33204332+takeofuture@users.noreply.github.com>
2026-02-08 14:49:41 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+OPENAI_API_KEY = "sk-proj-yourkey!"
+#LLM_PROVIDER="vllm"
+LLM_PROVIDER="openai" #LLM Provider 
+LLM_BASE_URL="http://192.168.1.200:8000/v1" #ローカルで動かしたときのWEBソケットURL
+LLM_MODEL="Qwen/Qwen2.5-14B-Instruct-AWQ" #ローカルで動かしたときのモデル名
+LLM_API_KEY="DUMMY" #vllmをAPIで動かしたときのキー　所有するサーバーで動かすときは不要
